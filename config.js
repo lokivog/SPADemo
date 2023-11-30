99964299bb98f9dcbe1182f50d76f2a6c4bb5742
lokivog <29866904-lokivog@users.noreply.replit.com>
2023-11-30 15:05:08 +0000

    Initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+// You need to provide your own values for these two settings.
+// See the comments at the top of index.html.
+const APP_ROOT = 'https://SPADemo--msfapi.repl.co';
+const CLIENT_ID = 'test-demo-spa';
+
+// Calculate frequently-used URLs from the above:
+const APP_HOME = `${APP_ROOT}/demo.html`;
+const APP_CALLBACK = `${APP_ROOT}/redirect.html`;
